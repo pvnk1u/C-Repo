87f77a7c9f43d289f70d3da8bdbe228eab35870c
pvnk1u <pvnk1u@gmail.com>
2024-06-26 13:14:14 +0800

    scanf函数和模式匹配

---
+++ chapter3/printf_example.c
@@ -0,0 +1,13 @@
+//
+// Created by pvnk1u on 2024/6/25.
+//
+#include <stdio.h>
+
+int main(void) {
+    printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");
+
+    printf("\"Hello!\"");
+
+    printf("\n");
+    printf("\\");
+}
