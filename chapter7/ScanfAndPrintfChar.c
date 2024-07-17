c37fd69f65f815c911aa16dd8d336d5591b03ba4
pvnk1u <pvnk1u@gmail.com>
2024-07-17 23:52:43 +0800

    字符类型

---
+++ chapter7/ScanfAndPrintfChar.c
@@ -0,0 +1,18 @@
+/*
+ Created by pvnk1u on 2024/7/17.
+*/
+
+
+#include <stdio.h>
+
+int main(void){
+
+    char ch;
+    scanf(" %c",&ch);
+    printf("%c",'S');
+    printf("%c",ch);
+    printf("%c",'S');
+    printf(" %c",ch);
+    printf("%c",'S');
+    return 0;
+}
