c1d31431f08a1f283bc81d810dfefa2ca1937220
pvnk1u <pvnk1u@gmail.com>
2024-08-22 13:08:21 +0800

    函数

---
+++ chapter9/average.c
@@ -0,0 +1,21 @@
+/*
+ Created by pvnk1u on 2024/8/22.
+计算三个浮点数的平均值
+*/
+
+#include <stdio.h>
+
+double average(double a,double b) {
+    return (a+b) /2;
+}
+
+int main(void) {
+    double x,y,z;
+
+    printf("Enter three numbers: ");
+    scanf("%lf%lf%lf",&x,&y,&z);
+    printf("Average of %g and %g: %g\n",x,y,average(x,y));
+    printf("Average of %g and %g: %g\n",y,z,average(y,z));
+    printf("Average of %g and %g: %g\n",x,z,average(x,z));
+    return 0;
+}
