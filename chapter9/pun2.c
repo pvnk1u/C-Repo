fe8f9135772278723ee85c9c12594387a03b299c
pvnk1u <pvnk1u@gmail.com>
2024-08-23 13:08:00 +0800

    函数

---
+++ chapter9/pun2.c
@@ -0,0 +1,14 @@
+/*
+ Created by pvnk1u on 2024/8/23.
+*/
+
+#include<stdio.h>
+
+void print_pun(void) {
+    printf("To C,or not to C: that is the question.\n");
+}
+
+int main(void){
+    print_pun();
+    return 0;
+}
